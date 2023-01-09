eced20fc5e45b100129cf5298806255918397d2b
Lene Lefiti <47546203+LenLen684@users.noreply.github.com>
2023-01-09 07:18:46 -0700

    Looking for low level mouse hooks
    Might have to change to WPF for this

--- StreamAvatar/MainPage.xaml.cs
+++ StreamAvatar/MainPage.xaml.cs
@@ -1,24 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Storage.Pickers;
-using Windows.Storage;
-using Windows.Storage.Streams;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
-using Windows.UI.Xaml.Shapes;
 using System.Numerics;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -37,19 +25,23 @@ namespace StreamAvatar
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
 
             //It's best to add all the images used and then flip which ones are active
+            //Application.Run(new ApplicationContext());
+
+            //This only works for when the cursor passes over the window:
+            //Window.Current.CoreWindow.PointerMoved += CoreWindowMoveImage;
         }
 
         //private void MoveCursor()
         //{
         //    Point pointerPosition = Windows.UI.Core.CoreWindow.GetForCurrentThread().PointerPosition;
-            
+
         //    //assuming these 
         //    int swidth = horizontal;
         //    int sheight = vertical;
 
         //    xletter = 0;
         //    yletter = 0;
-            
+
 
         //    float x = 0;
         //    float y = 0;
@@ -75,6 +67,8 @@ namespace StreamAvatar
             img.Height = 50;
             img.Width = 50;
             img.Source = bimg;
+            
+            //Bugs the image adding
             img.PointerMoved += MoveImage;
 
             Canvas.SetLeft(img, e.GetPosition(imageArea).X - img.Width / 2);
@@ -87,6 +81,15 @@ namespace StreamAvatar
             Point cursorPosition = Windows.UI.Core.CoreWindow.GetForCurrentThread().PointerPosition;
             Vector3 cursorInWindow = new Vector3((float)(cursorPosition.X - Window.Current.Bounds.X), (float)(cursorPosition.Y - Window.Current.Bounds.Y), 0);
             ((Image) sender).Translation = cursorInWindow;
+        }        
+        
+        private void CoreWindowMoveImage(CoreWindow sender, PointerEventArgs e)
+        {
+            Point cursorPosition = Windows.UI.Core.CoreWindow.GetForCurrentThread().PointerPosition;
+            Vector3 cursorInWindow = new Vector3((float)(cursorPosition.X - Window.Current.Bounds.X), (float)(cursorPosition.Y - Window.Current.Bounds.Y), 0);
+
+
+            mouseImage.Translation = cursorInWindow;
         }
     }
 }
